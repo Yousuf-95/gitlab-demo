a08073de2a739787460d8c4f6e3d32d3dc03d620
Yousuf Suhail <yf.yousuf95@gmail.com>
2022-04-14 06:39:07 +0530

    Made changes to deploy automatically to heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req,res) => {
-
     return res.send('<h3>Hello from the server.</h3>')
-})
+});
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(PORT, () => console.log('Server listening on port ' + PORT));
